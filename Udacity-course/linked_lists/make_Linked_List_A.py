326d6bc6c25b7481fa3f2d04f3092b9781f2b29f
bsegers <briansegs@gmail.com>
2024-05-21 09:53:14 -0400

    Add: create linked list A

---
+++ Udacity-course/linked_lists/make_Linked_List_A.py
@@ -0,0 +1,48 @@
+from Node import Node
+
+def create_linked_list(input_list):
+    head = None
+    if len(input_list) > 0:
+        for value in input_list:
+            if head is None:
+                head = Node(value)
+            else:
+                cur_node = head
+
+                while cur_node.next:
+                    cur_node = cur_node.next
+
+                cur_node.next = Node(value)
+    return head
+
+
+# Tests
+def test_function(input_list, head):
+    try:
+        if len(input_list) == 0:
+            if head is not None:
+                print("Fail")
+                return
+        for value in input_list:
+            if head.value != value:
+                print("Fail")
+                return
+            else:
+                head = head.next
+        print("Pass")
+    except Exception as e:
+        print("Fail: "  + e)
+
+
+
+input_list = [1, 2, 3, 4, 5, 6]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = [1]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = []
+head = create_linked_list(input_list)
+test_function(input_list, head)
