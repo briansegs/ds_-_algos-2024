08017592f3c7d3f28be2ef6eff8831f05ffe17af
bsegers <briansegs@gmail.com>
2024-05-23 10:34:12 -0400

    Add: design_browser_history

---
+++ Udacity-course/linked_lists/design_browser_history.py
@@ -0,0 +1,51 @@
+class DNode:
+    def __init__(self, val):
+        self.val = val
+        self.nxt = None
+        self.prev = None
+
+class BrowserHistory:
+    def __init__(self, homepage: str):
+        self.head = DNode(homepage)
+        self.curr = self.head
+        self.tail = self.curr
+
+    def visit(self, url: str) -> None:
+        self.curr.nxt = DNode(url)
+        self.curr.nxt.prev = self.curr
+        self.curr = self.curr.nxt
+
+    def back(self, steps: int) -> str:
+        count = 0
+        while count < steps:
+            if self.curr.prev is None:
+                return self.curr.val
+            self.curr = self.curr.prev
+            count += 1
+        return self.curr.val
+
+    def forward(self, steps: int) -> str:
+        count = 0
+        while count < steps:
+            if self.curr.nxt is None:
+                return self.curr.val
+            self.curr = self.curr.nxt
+            count += 1
+        return self.curr.val
+
+
+# Tests
+browser = BrowserHistory("leetcode.com")
+print('expected output: [null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]')
+print("actual output: ",
+    browser.visit("google.com"),
+    browser.visit("facebook.com"),
+    browser.visit("youtube.com"),
+    browser.back(1),
+    browser.back(1),
+    browser.forward(1),
+    browser.visit("linkedin.com"),
+    browser.forward(2),
+    browser.back(2),
+    browser.back(7),
+)
