d08bae71f564aa64f42684a726caf8d47e8a59f4
bsegers <briansegs@gmail.com>
2024-05-27 15:55:20 -0400

    Add: insert method

--- Udacity-course/linked_lists/linked_list_practice.py
+++ Udacity-course/linked_lists/linked_list_practice.py
@@ -136,3 +136,40 @@ value = linked_list.pop()
 assert value == 2, f"list contents: {linked_list.to_list()}"
 assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
 print('pass')
+
+# insert
+
+def insert(self, value, pos):
+    """ Insert value at pos position in the list. If pos is larger than the
+    length of the list, append to the end of the list. """
+
+    # TODO: Write function to insert here    
+    curr_pos = 0
+    curr = self.head
+    dummy = Node(0)
+    dummy.next = curr
+    prev = dummy
+    while curr:
+        if curr_pos == pos:
+            prev.next = Node(value)
+            prev.next.next = curr
+            break
+        elif curr.next is None:
+            curr.next = Node(value)
+            break
+        else:
+            prev = curr
+            curr = curr.next
+            curr_pos += 1
+    self.head = dummy.next
+
+LinkedList.insert = insert
+
+# Test insert
+linked_list.insert(5, 0)
+assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
+linked_list.insert(2, 1)
+assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+linked_list.insert(3, 6)
+assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+print("pass")
