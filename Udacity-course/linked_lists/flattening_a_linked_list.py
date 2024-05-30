6c39c7ac9055047b6df9c56f9e866d2a043f3df9
bsegers <briansegs@gmail.com>
2024-05-30 18:19:30 -0400

    add: Alternate solution

--- Udacity-course/linked_lists/flattening_a_linked_list.py
+++ Udacity-course/linked_lists/flattening_a_linked_list.py
@@ -155,3 +155,85 @@ expected_list = [1,2,2,3,4,4,5]
 # Convert the "solution" into a Python list and compare with another Python list
 assert solution.to_list() == expected_list, f"list contents: {solution.to_list()}"
 print("pass")
+
+
+# Alternate solution
+
+def merge(list1, list2):
+    merged = LinkedList(None)
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
+    list1_elt = list1.head
+    list2_elt = list2.head
+    while list1_elt is not None or list2_elt is not None:
+        if list1_elt is None:
+            merged.append(list2_elt)
+            list2_elt = list2_elt.next
+        elif list2_elt is None:
+            merged.append(list1_elt)
+            list1_elt = list1_elt.next
+        elif list1_elt.value <= list2_elt.value:
+            merged.append(list1_elt)
+            list1_elt = list1_elt.next
+        else:
+            merged.append(list2_elt)
+            list2_elt = list2_elt.next
+    return merged
+
+''' Test merge() function'''
+linked_list = LinkedList(Node(1))
+linked_list.append(3)
+linked_list.append(5)
+
+second_linked_list = LinkedList(Node(2))
+second_linked_list.append(4)
+
+merged = merge(linked_list, second_linked_list)
+node = merged.head
+while node is not None:
+    #This will print 1 2 3 4 5
+    print(node.value)
+    node = node.next
+
+# Lets make sure it works with a None list
+merged = merge(None, linked_list)
+node = merged.head
+while node is not None:
+    #This will print 1 3 5
+    print(node.value)
+    node = node.next
+
+''' In a NESTED LinkedList object, each node will be a simple LinkedList in itself'''
+class NestedLinkedList(LinkedList):
+    def flatten(self):
+        return self._flatten(self.head) # <-- self.head is a node for NestedLinkedList
+
+    '''  A recursive function ''' 
+    def _flatten(self, node):
+
+        # A termination condition
+        if node.next is None:
+            return merge(node.value, None) # <-- First argument is a simple LinkedList
+
+        # _flatten() is calling itself untill a termination condition is achieved
+        return merge(node.value, self._flatten(node.next)) # <-- Both arguments are a simple LinkedList each
+
+''' Test flatten() function'''
+# Create a nested linked list with one node. 
+# The node itself is a simple linked list as 1-->3-->5 created previously
+nested_linked_list = NestedLinkedList(Node(linked_list))
+
+# Append a node (a linked list as 2-->4) to the existing nested linked list
+nested_linked_list.append(second_linked_list)
+
+# Call the `flatten()` function
+flattened = nested_linked_list.flatten()
+
+# Logic to print the flattened list
+node = flattened.head
+while node is not None:
+    #This will print 1 2 3 4 5
+    print(node.value)
+    node = node.next
